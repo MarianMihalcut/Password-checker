f2b467195412cb13a9a50e782a1b8ca60070ef21
MarianMihalcut <marianmihalcut@gmail.com>
2024-07-26 13:46:01 +0300

    Wrote functions for initial state

--- pass_check.h
+++ pass_check.h
@@ -1,8 +1,11 @@
 #ifndef PASS_CHECK_H_
 #define PASS_CHECK_H_
 
+//initial state
 char *password_input();
-void init_chr();
-
+char *init_digits();
+char *init_letters_no_caps();
+char *init_letters_caps();
+char *init_special_chr();
 
 #endif
