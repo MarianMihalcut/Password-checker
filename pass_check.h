084874617e0b355cdea85861f3bcaa3f32c5d8d1
MarianMihalcut <marianmihalcut@gmail.com>
2024-07-31 11:26:12 +0300

    New functions for checking characters

--- pass_check.h
+++ pass_check.h
@@ -8,4 +8,14 @@ const char *init_letters_no_caps();
 const char *init_letters_caps();
 const char *init_special_chr();
 
+//for exit
+bool exit();
+
+//password
+short int pass_length(char *&password);
+bool has_digits(char *&password,const char *&digits);
+bool has_small_letters(char *&password,const char *&letters);
+bool has_cap_letters(char *&password,const char *&letters);
+bool has_special_chr(char *&password);
+
 #endif
